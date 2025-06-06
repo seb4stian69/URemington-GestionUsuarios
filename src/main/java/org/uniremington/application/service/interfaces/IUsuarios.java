536354137b9895fee628bcb61c5ee5dbbb3ad16a
Sebastian Santis <sebastian.santis@sofka.com.co>
2025-06-06 13:31:50 -0500

    Agregando logica de dominio en la aplicacion

--- src/main/java/org/uniremington/application/service/interfaces/IUsuarios.java
+++ src/main/java/org/uniremington/application/service/interfaces/IUsuarios.java
@@ -1,14 +1,16 @@
 package org.uniremington.application.service.interfaces;
 
 import org.uniremington.domain.model.Usuario;
+import org.uniremington.shared.util.ApiResponse;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IUsuarios {
     Optional<Usuario> findById(Long id);
-    Optional<Usuario> login(String user, String contrasena);
+    Optional<Usuario> login(String user);
     List<Usuario> findAll();
     Usuario save(Usuario entity);
     void deleteById(Long id);
+    ApiResponse resetPassword(String username);
 }
