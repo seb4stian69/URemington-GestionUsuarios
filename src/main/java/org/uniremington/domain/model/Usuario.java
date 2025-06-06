361ef792a9deefcf10f6695daf763340fab2befa
Sebastian Santis <sebastian.santis@sofka.com.co>
2025-06-06 12:07:51 -0500

    Modificando clase de usuario

--- src/main/java/org/uniremington/domain/model/Usuario.java
+++ src/main/java/org/uniremington/domain/model/Usuario.java
@@ -1,13 +1,17 @@
 package org.uniremington.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.uniremington.shared.base.BaseEntity;
 
-@Data
+@Data/**/@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class Usuario extends BaseEntity {
     private Long id;
     private String nombreUsuario;
     private String contrasena;
+    private String idPerfil;
+    private String salt;
+    private Boolean estado;
 }
