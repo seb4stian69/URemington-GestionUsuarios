793051729d0f0d8c9ee8c5f1a208876f937ade09
Sebastian Santis <sebastian.santis@sofka.com.co>
2025-06-06 12:07:08 -0500

    Agregando logica de negocio de reseteo de password

--- src/main/java/org/uniremington/domain/repository/UsuarioRepository.java
+++ src/main/java/org/uniremington/domain/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package org.uniremington.domain.repository;
 
 import org.uniremington.domain.model.Usuario;
 import org.uniremington.shared.base.BaseRepository;
+import org.uniremington.shared.util.ApiResponse;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends BaseRepository<Usuario, Long> {
-    Optional<Usuario> login(String user, String contrasena);
+    Optional<Usuario> login(String user);
+    ApiResponse resetPassword(String username);
 }
